9dbb7173262cdb4d5682ddfc61c9315bdc6c98b9
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2026-02-13 18:16:08 +0900

    ・パイプラインの設定をプログラムに反映するロジックを追加する。

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+import sys
 
+pipeline_options = PipelineOptions(sys.argv)
 with beam.Pipeline() as p:
 
   (p | beam.Create(range(1, 11))
